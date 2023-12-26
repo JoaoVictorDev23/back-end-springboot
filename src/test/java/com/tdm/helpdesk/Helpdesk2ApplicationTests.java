a6554d56e6ce08a4868304e95e4f3882af01cafb
JoaoVictorDev23 <joaovictor500@gmail.com>
2023-12-26 14:01:35 -0300

    projeto iniciado

---
+++ src/test/java/com/tdm/helpdesk/Helpdesk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdm.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helpdesk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
