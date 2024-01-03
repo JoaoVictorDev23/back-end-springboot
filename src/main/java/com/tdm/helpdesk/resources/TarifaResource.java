b4a05925024a3ef87dd91b5704e0ef7971e0f122
JoaoVictorDev23 <joaovictor500@gmail.com>
2024-01-03 16:05:22 -0300

    FindAll para os chamados

--- src/main/java/com/tdm/helpdesk/resources/TarifaResource.java
+++ src/main/java/com/tdm/helpdesk/resources/TarifaResource.java
@@ -1,5 +1,8 @@
 package com.tdm.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,14 @@ public class TarifaResource {
 	public ResponseEntity<TarifaDTO> findById(@PathVariable Integer id){
 		Tarifa obj = service.findById(id);
 		return ResponseEntity.ok().body(new TarifaDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TarifaDTO>> findAll(){
+		List<Tarifa> list = service.findAll();
+		List<TarifaDTO> listDTO = list.stream().map(obj -> new TarifaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
 	}
 
 }
--- src/main/java/com/tdm/helpdesk/services/TarifaService.java
+++ src/main/java/com/tdm/helpdesk/services/TarifaService.java
@@ -1,5 +1,6 @@
 package com.tdm.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class TarifaService {
 	public Tarifa findById(Integer id) {
 		Optional<Tarifa> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! ID:" +id ));
+	}
+
+
+	public List<Tarifa> findAll() {
+		return repository.findAll();
 	}
 
 }
